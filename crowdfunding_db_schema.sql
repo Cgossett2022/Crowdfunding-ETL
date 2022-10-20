3063223518aebefec8458b87622dd9fada0c246a
Clayton Gossett <111243284+Cgossett2022@users.noreply.github.com>
2022-10-19 22:02:51 -0500

    Corrected Crowdfunding_db_schema file

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -16,7 +16,7 @@ CREATE TABLE "campaign" (
     "launch_date" date   NOT NULL,
     "end_date" date   NOT NULL,
     "category_id" varchar(10)   NOT NULL,
-    "subcategory_id" varchar(10)   NOT NULL,
+    "sub-category_id" varchar(10)   NOT NULL,
     CONSTRAINT "pk_campaign" PRIMARY KEY (
         "cf_id"
      )
@@ -26,25 +26,34 @@ CREATE TABLE "contacts" (
     "contact_id" int   NOT NULL,
     "first_name" varchar(50)   NOT NULL,
     "last_name" varchar(50)   NOT NULL,
-    "email" varchar(100)   NOT NULL
+    "email" varchar(100)   NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
+     )
 );
 
 CREATE TABLE "category" (
     "category_id" varchar(10)   NOT NULL,
-    "category_name" varchar(50)   NOT NULL
+    "category_name" varchar(50)   NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "category_id"
+     )
 );
 
 CREATE TABLE "subcategory" (
     "subcategory_id" varchar(10)   NOT NULL,
-    "subcategory_name" varchar(50)   NOT NULL
+    "subcategory_name" varchar(50)   NOT NULL,
+    CONSTRAINT "pk_subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
 );
 
 CREATE TABLE "backers" (
-    "backer_id" varchar(50)   NOT NULL,
+    "backer_id" varchar(10)   NOT NULL,
     "cf_id" int   NOT NULL,
     "first_name" varchar(50)   NOT NULL,
     "last_name" varchar(50)   NOT NULL,
-    "email" varchar(100)   NOT NULL,
+    "email" varchar(50)   NOT NULL,
     CONSTRAINT "pk_backers" PRIMARY KEY (
         "backer_id"
      )
@@ -56,18 +65,8 @@ REFERENCES "contacts" ("contact_id");
 ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
 REFERENCES "category" ("category_id");
 
-ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
-REFERENCES "subcategory" ("subcategory_id");
-
-ALTER TABLE "contacts" ADD CONSTRAINT "fk_contacts_contact_id" FOREIGN KEY("contact_id")
-REFERENCES "contacts" ("contact_id");
-
-ALTER TABLE "category" ADD CONSTRAINT "fk_category_category_id" FOREIGN KEY("category_id")
-REFERENCES "category" ("category_id");
-
-ALTER TABLE "subcategory" ADD CONSTRAINT "fk_subcategory_subcategory_id" FOREIGN KEY("subcategory_id")
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_sub-category_id" FOREIGN KEY("sub-category_id")
 REFERENCES "subcategory" ("subcategory_id");
 
 ALTER TABLE "backers" ADD CONSTRAINT "fk_backers_cf_id" FOREIGN KEY("cf_id")
 REFERENCES "campaign" ("cf_id");
-
